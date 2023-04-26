315408ae857e33d4a825cebcd9507c043fa08bb1
Alice Souza <alicefms@hotmail.com>
2023-04-26 18:06:28 -0300

    Configurações iniciais: add dependência do Selenium, e importar o WebDriver

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class main {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+        driver.quit();
+    }
+
+}
